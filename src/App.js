30e01b1ea6e7fa9fb3908bf09cab5fcabe5208ed
Joe Fuentes <job@joefuentes.com>
2023-09-22 23:26:38 -0700

    move to main folder

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+import './App.css';
+import { TicTacToe } from './TicTacToe'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TicTacToe />
+      </header>
+    </div>
+  );
+}
+
+export default App;
